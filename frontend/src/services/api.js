db65450bff58647b57553f7b6910d95b1f1f0416
aishwaryapj05-svg <aishwaryapj05@gmail.com>
2026-04-04 20:04:37 +0530

    running

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://backend-service:5000/api",
+  baseURL: "http://backend-service:3000/api",
   withCredentials: true
 });
 
